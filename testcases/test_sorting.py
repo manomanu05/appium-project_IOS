aae7a01c9533faebf6c3281adf803cbbb73ca016
manomanu05 <153796220+manomanu05@users.noreply.github.com>
2025-10-27 16:05:22 +0530

    Update test_sorting.py

--- testcases/test_sorting.py
+++ testcases/test_sorting.py
@@ -3,13 +3,13 @@ from PageObject.LoginPage import LoginPage
 from PageObject.SortingPage import SortingPageIOS  # iOS page object
 
 def test_login_and_apply_filter_ios(driver):
-    # Step 1: Login
+    # Login
     login_page = LoginPage(driver)
     login_page.login("standard_user", "secret_sauce")
 
-    # Step 2: Open filter modal
+    #  Open filter modal
     sorting_page = SortingPageIOS(driver)
     sorting_page.open_filter_modal()
 
-    # Step 3: Select "Price (low to high)"
+    #  Select "Price (low to high)"
     sorting_page.select_price_low_to_high()
