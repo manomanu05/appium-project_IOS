8c2858c217a8d1c088222fef3fa00336681df69e
manomanu05 <153796220+manomanu05@users.noreply.github.com>
2025-10-27 16:06:04 +0530

    Update ReadConfig.py

--- utilities/ReadConfig.py
+++ utilities/ReadConfig.py
@@ -4,7 +4,6 @@ import os
 class ReadConfig:
     @staticmethod
     def get_config(section, key):
-        """Read string value from config.ini"""
         config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Configurations', 'config.ini'))
         config = configparser.ConfigParser()
         config.read(config_path)
@@ -12,7 +11,6 @@ class ReadConfig:
 
     @staticmethod
     def get_bool(section, key):
-        """Read boolean value from config.ini"""
         config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Configurations', 'config.ini'))
         config = configparser.ConfigParser()
         config.read(config_path)
